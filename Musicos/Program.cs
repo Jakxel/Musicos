111ba0ae62a588f9cd9780c8b52a4bb9e20cf903
Victor Jakxel Islas Carreon <124114440+Jakxel@users.noreply.github.com>
2023-03-24 19:07:51 +0000

    codigo Terminado

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,38 +1,76 @@
-﻿abstract class Musico
-{
-    public string? Nombre {get; set;}
-    public Musico(string n){Nombre = n;}
-    public string? Instrumentos {get; set;}
-    public void Saludar(){Console.WriteLine($"Hola soy {Nombre}");}
-    public /*virtual*/ abstract void Tocar(){Console.WriteLine($"{Nombre} tocando su isntrumento");}
-   
-}
- class Bajista : Musico
-    {
-        public string bajo{get; set;}
-        public Bajista(string n, string b): base(n)
-        {
-            bajo=b;
-        }
-        public override void Tocar()
-        {
-            Console.WriteLine($"{Nombre}tocando su {bajo}");
-        }
-    }
+﻿abstract class Musico //clases abstactas solo pueden generar subclases, pero no objetos directamente.
+{                     //clases no abstractas admiten polimorfismo en sus objetos.
+    public string Nombre {get; set;}
 
+    public Musico(string nombre) => Nombre = nombre;
+
+    public virtual string GetSaludo() => "Hola, soy " + Nombre;
+
+     public virtual void Saludar() => Console.WriteLine(GetSaludo()); //refactorizando el saludo para reutilizar.
+
+    public /*virtual*/ abstract void Tocar(); // Console.WriteLine($"{Nombre} está tocando su instrumento");
+
+    /*  Metodos abstractos: Deben estar en clases abstractas para existir. 
+                            Metodos abstractos no tienen implementación.
+                            Deben obligatoriamente redefinirse en subclases
+     
+        Metodos virtuales:  Dan la opción de redefinirse en subclases
+            */
+}
+
+
+
+class Baterista:Musico
+{
+    public string Bateria {get; set;}
+
+    public Baterista(string nombre, string bateria):base(nombre) => Bateria = bateria;
+
+    public override /*new*/ void Tocar() => Console.WriteLine($"{Nombre} está tocando su {Bateria}");
+    //para implementar metodos abstractos debemos de usar la palabra reservada override (redefinir/sobreescribir)
+
+    public override string GetSaludo() => base.GetSaludo() + " y soy baterísta" ;
+    public override void Saludar() => Console.WriteLine(GetSaludo());
+}
+
+class Guitarrista:Musico
+{
+    public string Guitarra {get; set;}
+
+    public Guitarrista(string nombre, string guitarra):base(nombre) => Guitarra = guitarra;
+
+    public override /*new*/ void Tocar() => Console.WriteLine($"{Nombre} está tocando su {Guitarra}");
+    //para implementar metodos abstractos debemos de usar la palabra reservada override (redefinir/sobreescribir)
+
+     public override string GetSaludo() => base.GetSaludo() + " y soy guitarrista, che" ;
+    public override void Saludar() => Console.WriteLine(GetSaludo());
+}
+
+class Bajista:Musico
+{
+    public string Bajo {get; set;}
+
+    public Bajista(string nombre, string bajo):base(nombre) => Bajo = bajo;
+
+    public override void Tocar() => Console.WriteLine($"{Nombre} está tocando su {Bajo}");
+
+     public override string GetSaludo() => base.GetSaludo() + " y lamentablemente soy bajista" ;
+    public override void Saludar() => Console.WriteLine(GetSaludo());
+}
 
 class Program
 {
-    private static void Main(string[] args)
+    static void Main()
     {
-        List<Musico>grupo=new List<Musico>();
-        grupo.Add(new Musico("Chalino Sanchez"));
-        grupo.Add(new Bajista("El bajista de Cjhalino Sanchez", "Bajo"));
+       List<Musico> SodaStereo = new List<Musico>(); //Se puede crear listas de clases abstractas. (referencias)
 
-        foreach(var m in grupo)
-            m.Saludar();
-
-        foreach(var m in grupo)
-            m.Tocar();
+       SodaStereo.Add(new Guitarrista("Gustavo Cerati", "Gibson"));
+       SodaStereo.Add(new Bajista("Zeta", "MusicMan"));
+       SodaStereo.Add(new Baterista("Charlie", "Tama")); 
+       
+       foreach(var m in SodaStereo) m.Saludar();
+                  
+       foreach(var m in SodaStereo) m.Tocar(); /*Polimorfismo: Los objetos pueden variar su comportamiento
+                                                               dependiendo del contexto. (clase/subclase)*/
     }
 }
